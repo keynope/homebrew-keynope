f13cdc1ef11dbf1cfd692c52d44fd31da7fea6d7
Dennis Vink <dennis.vink@xebia.com>
2026-07-23 22:28:46 +0200

    Update 0.1.3 release checksums

--- Casks/keynope.rb
+++ Casks/keynope.rb
@@ -2,8 +2,8 @@ cask "keynope" do
   version "0.1.3"
   arch arm: "arm64", intel: "amd64"
 
-  sha256 arm:   "39a20689ed7a58f9403a1110e8d3a08074e5e9428a6af7fc4e5040b90e10217c",
-         intel: "122c38889e363cb37cd5436451352666e68c062b6b087a415e00dfbaff92583a"
+  sha256 arm:   "6ddf3ba48b9252b7f3b9444d5e4d5d9c5ba0a57d40bce60b682a17c9eb7ea033",
+         intel: "ddaf44b5da00ebdbdb6e1c5ee93afd3ebd38af0014a764eea5a2a34044e2b4d3"
 
   url "https://github.com/keynope/keynope/releases/download/#{version}/keynope-mac-#{arch}.tar.gz",
       verified: "github.com/keynope/keynope/"
